e27b95b441c76a3195789a0e503c48dfe667e745
Amit12220 <ajha.9614938@gmail.com>
2022-09-11 20:01:45 +0530

    hi

---
+++ check/che.java
@@ -0,0 +1,22 @@
+package com.check;
+
+import java.util.Scanner;
+
+public class che {
+
+
+        public static void main(String []args)
+        {
+            String name=new String();
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter Name");
+            name=sc.next();
+            String add=new String();
+            System.out.println("Enter Address");
+            add=sc.next();
+            System.out.println(name);
+            System.out.println(add);
+        }
+
+
+}
---
+++ check/sec.java
@@ -0,0 +1,19 @@
+package com.check;
+
+public class sec {
+    public static void main(String[] args) {
+        //default data type for decimal nums is double
+        //byte<int
+        //float<double
+        double d= 5.35;
+        float r=(float) d;
+        //casting: 2 ways
+        //1st way
+        float f= (float) 5.35;
+        //2nd way: shortcut
+        float g=5.35f;
+        float l=(float) 5.2;
+        double q=l;
+        System.out.println(r);
+    }
+}
